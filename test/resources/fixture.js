39c523f00a416b560d7846ac6d76cf165c707f90
David Luecke <daff@neyeon.com>
2015-06-06 10:53:44 -0600

    Socket.io and Primus clients working and tested.

--- test/resources/fixture.js
+++ test/resources/fixture.js
@@ -4,7 +4,20 @@ var utils = require('../../lib/utils');
 // An in-memory service implementation
 var memory = require('feathers-memory');
 
-module.exports = function() {
+Object.defineProperty(Error.prototype, 'toJSON', {
+  value: function () {
+    var alt = {};
+
+    Object.getOwnPropertyNames(this).forEach(function (key) {
+      alt[key] = this[key];
+    }, this);
+
+    return alt;
+  },
+  configurable: true
+});
+
+module.exports = function(configurer) {
   // Create an in-memory CRUD service for our Todos
   var todoService = memory().extend({
     get: function(id, params, callback) {
@@ -21,7 +34,6 @@ module.exports = function() {
   var app = feathers()
     // Set up REST and SocketIO APIs
     .configure(feathers.rest())
-    .configure(feathers.socketio())
     // Parse HTTP bodies
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: true }))
@@ -29,6 +41,10 @@ module.exports = function() {
     .use(feathers.static(__dirname))
     // Host our Todos service on the /todos path
     .use('/todos', todoService);
+
+  if(typeof configurer === 'function') {
+    configurer.call(app);
+  }
 
   app.service('todos').create({ text: 'some todo', complete: false }, {}, function() {});
 
