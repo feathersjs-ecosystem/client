9d94e4c35f4f038eb10c6139aa78e8fb4d69f0d5
David Luecke <daff@neyeon.com>
2016-03-03 20:52:54 -0800

    Update src

--- src/client.js
+++ src/client.js
@@ -2,8 +2,8 @@ import feathers from 'feathers/client';
 import socketio from 'feathers-socketio/client';
 import primus from 'feathers-primus/client';
 import rest from 'feathers-rest/client';
-import authentication from 'feathers-authentication/client';
 import hooks from 'feathers-hooks';
+import authentication from 'feathers-authentication/client';
 
 Object.assign(feathers, { socketio, primus, rest, hooks, authentication });
 
