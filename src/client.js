c009f7d1d7b40bc69b57626237f637e26a7de090
David Luecke <daff@neyeon.com>
2016-03-03 19:34:10 -0800

    Adding feathers-authentication to client

--- src/client.js
+++ src/client.js
@@ -2,8 +2,9 @@ import feathers from 'feathers/client';
 import socketio from 'feathers-socketio/client';
 import primus from 'feathers-primus/client';
 import rest from 'feathers-rest/client';
+import authentication from 'feathers-authentication/client';
 import hooks from 'feathers-hooks';
 
-Object.assign(feathers, { socketio, primus, rest, hooks });
+Object.assign(feathers, { socketio, primus, rest, hooks, authentication });
 
 export default feathers;
