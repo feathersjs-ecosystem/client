1951ce22867c982a9f81a38de6ffd968d2bb06ab
Matthew Ratzke <matt@degree9.io>
2017-01-11 09:45:12 -0700

    Feathers Auth Update (#131)
    * Update feathers-authentication-client version
    
    * Update feathers-authentication-client
    
    * Bump version
    
    * Updating dist
    
    * Revert "Updating dist"
    
    This reverts commit ac871b0a87373e15160104f179aece00cdf7b7c7.
    
    * Revert "Bump version"
    
    This reverts commit 0f857a3659a556d421af5272ef86fb723fe8d52e.

--- src/client.js
+++ src/client.js
@@ -3,7 +3,7 @@ import rest from 'feathers-rest/client';
 import socketio from 'feathers-socketio/client';
 import primus from 'feathers-primus/client';
 import hooks from 'feathers-hooks';
-import authentication from 'feathers-authentication/client';
+import authentication from 'feathers-authentication-client';
 
 Object.assign(feathers, { socketio, primus, rest, hooks, authentication });
 
