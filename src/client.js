4ee7a33245a17ef36eff855368994a6b8280fa98
David Luecke <daff@neyeon.com>
2017-03-07 19:31:11 -0800

    Add feathers-errors to the client export (#152)

--- src/client.js
+++ src/client.js
@@ -1,10 +1,18 @@
 import feathers from 'feathers/client';
 import hooks from 'feathers-hooks';
+import errors from 'feathers-errors';
 import authentication from 'feathers-authentication-client';
 import rest from 'feathers-rest/client';
 import socketio from 'feathers-socketio/client';
 import primus from 'feathers-primus/client';
 
-Object.assign(feathers, { socketio, primus, rest, hooks, authentication });
+Object.assign(feathers, {
+  socketio,
+  primus,
+  rest,
+  hooks,
+  authentication,
+  errors
+});
 
 export default feathers;
